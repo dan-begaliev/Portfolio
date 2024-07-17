b1dbf9790519e91f9daad341f93cfe73b6860cbf
Aelita B <aelitab@Aelitas-Laptop.attlocal.net>
2024-07-17 00:37:08 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div id="home">
+        <Home />
+      </div>
+
+      <div id="about">
+        <About />
+      </div>
+
+      <div id="experience">
+        <Experience />
+      </div>
+
+      <div id="projects">
+        <Projects />
+      </div>
+
+      <div id="contact">
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
